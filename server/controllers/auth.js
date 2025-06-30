1e9d71af34dd9ab2f0fb0b0fd86f38f56e85b550
johnandrewsanvictores <sanvictoresjohnandrewe@gmail.com>
2025-06-30 09:49:22 +0800

    fix cookie

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -193,7 +193,7 @@ export const signIn = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production', // only on HTTPS in production
-            sameSite: 'Strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
